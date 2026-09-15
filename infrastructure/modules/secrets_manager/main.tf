a16a1fa373c4fe1f8dd0189e82443d47bbaadac8
Hari P S <hari.ps@experionglobal.local>
2026-09-15 09:11:29 +0530

    update

--- infrastructure/modules/secrets_manager/main.tf
+++ infrastructure/modules/secrets_manager/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_credentials" {
-name = "${var.environment}/database/n-credentials"
+name = "${var.project_name}-${var.environment}/database/n-credentials"
 recovery_window_in_days = 0
 }
 
